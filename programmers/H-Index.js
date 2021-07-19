62b6814c33d006d6b9cfb94cac37794dbd83788c
JuneHyung <cjh951114@gmail.com>
2021-07-19 19:44:43 +0900

    Create H-Index.js
    Create H-Index.js

---
+++ programmers/H-Index.js
@@ -0,0 +1,11 @@
+function solution(citations) {
+    citations.sort((a, b) => { return b - a });
+    
+    let answer = 0;
+    while (answer < citations[answer]) {
+        answer += 1;
+    }
+    return answer;
+}
+let citations = [3, 0, 6, 1, 5];
+console.log(solution(citations));
