5a550b0b43095c0e3a8332c0067c03cc613a82c4
JuneHyung <cjh951114@gmail.com>
2021-08-26 15:20:13 +0900

    Create 올바른괄호.js
    Create 올바른괄호.js

---
+++ programmers/올바른괄호.js
@@ -0,0 +1,19 @@
+function solution(s){
+  var answer = true;
+  let str = s.split('');
+  let stack = [];
+  
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == '(') stack.push(str[i]);
+    else if (stack.length != 0 || stack.pop(')')) answer = false;
+      
+      
+    // else {
+    //   if (stack.length == 0 || stack.pop() == ')') answer = false;
+    // }
+  }
+  if (stack.length != 0) answer = false;
+  return answer;
+}
+let s = ")";
+console.log(solution(s));
