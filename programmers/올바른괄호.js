27aea16931bf13403fa2cddd71fc435586f1e935
JuneHyung <cjh951114@gmail.com>
2021-08-26 16:29:38 +0900

    Update 올바른괄호.js
    Update 올바른괄호.js

--- programmers/올바른괄호.js
+++ programmers/올바른괄호.js
@@ -5,12 +5,10 @@ function solution(s){
   
   for (let i = 0; i < str.length; i++) {
     if (str[i] == '(') stack.push(str[i]);
-    else if (stack.length != 0 || stack.pop(')')) answer = false;
-      
-      
-    // else {
-    //   if (stack.length == 0 || stack.pop() == ')') answer = false;
-    // }
+    // else if (stack.length != 0 || stack.pop(')')) answer = false;      
+    else {
+      if (stack.length == 0 || stack.pop() == ')') answer = false;
+    }
   }
   if (stack.length != 0) answer = false;
   return answer;
