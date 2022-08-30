e36464ddedc3c689b09cc242567f9703f47495b2
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2022-08-30 22:23:42 +0900

    1-15 substr 추가

--- inflearn/practice/01-15. 가운데 문자 출력.js
+++ inflearn/practice/01-15. 가운데 문자 출력.js
@@ -1,5 +1,8 @@
 const solution = (str) => {
     const answer = str.length %2 ===0 ? `${str[Math.ceil(str.length/2)]}${str[Math.ceil(str.length/2)]}` : str[Math.ceil(str.length/2)-1] ;
+
+    // const mid = Math.floor(str.length/2);
+    // const answer = str.length %2 === 0 ? str.substr(mid-1, 2) : str.substr(mid, 1) 
     return answer;
 }
 
