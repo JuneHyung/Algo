4d12dd8a2ffbf4a268fce66728b85450cba128af
JuneHyung <cjh951114@gmail.com>
2022-10-30 17:48:37 +0900

    Update 선입선출스케줄링 : 주석추가

--- programmers/선입선출스케줄링.js
+++ programmers/선입선출스케줄링.js
@@ -12,6 +12,8 @@
 
 function solution(n, cores) {
   const coreLen = cores.length;
+  // var의 키워드가 let보다 빠르기 때문에 효율성통과를 위해 var로 선언. 
+  // n을 그대로 활용해서 별도의 변수 선언없이 사용해도 가능.
   var rest = n - coreLen; // 처리할 나머지 일 개수
   let lt = 1; // 최소
   let rt = Math.max(...cores) * rest / coreLen // 최대
@@ -22,19 +24,21 @@ function solution(n, cores) {
     for(const core of cores){
       capacity += Math.floor(mid / core);
     }
-
     capacity >=rest ? rt = mid : lt = mid+1;
   }
-
+  
   for(const core of cores){
     rest -= Math.floor((rt-1)/core);
   }
 
+  // 남은 rest수는 코어 개수를 초과할 수 없다.
+  // 위에서 현재 시간대에 처리할 수 있는 rest만 남겼다.
+  // 그렇기 때문에 코어개수만큼만 반복.
   for(let i=0;i<coreLen;i++){
     if(rt%cores[i]===0) {
-      rest -= 1;
-      if(!rest){
-        return i+1;
+      rest -= 1; // 새 작업을 할당하기 때문에 작업수 -1
+      if(!rest){ // 남은 작업이 0이 되면
+        return i+1; // 그때 인덱스를 반환.
       }
     }
   }
