8476abec324ec694589d02068be00dd59a3b52da
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2022-11-29 22:41:31 +0900

    외벽점검.js

--- programmers/외벽점검.js
+++ programmers/외벽점검.js
@@ -21,39 +21,48 @@
  * 1. weak만큼 돌면서 각  경로 추출
  * 2. 각 경로 첫 지점과 현재 투입 인원의 작업 범위를 더한 값이 해당 인원의 커버가능한 거리
  */
-const getPermutation = (arr, n) =>{
-  if(n===1) return arr.map(el=>[el]);
+ const getPermutation = (arr, n) => {
+  if (n === 1) return arr.map(el => [el]);
   const result = [];
-
-  arr.forEach((fixed, idx, origin)=>{
-    const rest = [...origin.slice(0, idx), ...origin.slice(idx+1)];
-    const perms = getPermutation(rest, n-1);
-    const attached = perms.map(perm=>[fixed, ...perm]);
+  arr.forEach((fixed, idx, origin) => {
+    const rest = [...origin.slice(0, idx), ...origin.slice(idx + 1)];
+    const perms = getPermutation(rest, n - 1);
+    const attached = perms.map(perm => [fixed, ...perm]);
     result.push(...attached);
-  });
+  })
   return result;
 }
-
 function solution(n, weak, dist) {
   const len = weak.length;
-  const linear_weak = Array.from({length: len*2 - 1}, ()=>0);
-  for(let i=0;i<len*2-1;i++){
-    linear_weak[i] = i<len ? weak[i] : weak[i-len] + n;
+  const linear_weak = Array.from({ length: 2 * len - 1 }, () => 0)
+  for (let i = 0; i < len * 2 - 1; i++) {
+    linear_weak[i] = i < len ? weak[i] : weak[i - len] + n;
   }
+  console.log(linear_weak);
 
-  dist.sort((a,b)=>b-a);
+  dist.sort((a, b) => b - a);
 
-  for(let i=1;i<=dist.length; i++){
+  for (let i = 1; i <= dist.length; i++) {
     const permutation = getPermutation(dist, i);
-    
-    for(const permu of permutation){
-      for(let j=0;j<len;j++){
-        let line = linear_weak.slice(j, len+j);
-        for(const p of permu){
+    console.log(i);
+    console.log(permutation);
+    console.log()
+    for (const permu of permutation) {
+      for (let j = 0; j < len; j++) {
+        let line = linear_weak.slice(j, j + len);
+        console.log('line')
+        console.log(line);
+
+        for (const p of permu) {
           const coverage = line[0] + p;
-          line = line.filter(e=> e>coverage);
-          if(!line.length) return i;
+          line = line.filter(e => e > coverage);
+          console.log('coverage');
+          console.log(coverage);
+          console.log(line);
+          console.log();
+          if (!line.length) return i;
         }
+
       }
     }
   }
@@ -63,5 +72,4 @@ function solution(n, weak, dist) {
 const n = 12;
 const weak = [1, 5, 6, 10];
 const dist = [1, 2, 3, 4];
-
 console.log(solution(n, weak, dist));
