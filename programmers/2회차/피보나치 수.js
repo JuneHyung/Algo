1b901e5637294f51e0927b0a5a17c0d079c2cd0e
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2023-01-05 23:51:42 +0900

    피보나치 수 2트

--- programmers/2회차/피보나치 수.js
+++ programmers/2회차/피보나치 수.js
@@ -1,12 +1,11 @@
+/**피보나치 */
 const solution = (n) =>{
-  let fibo = [0,1];
-  for(let i=0;i<n-1; i++){
-    let tmp = (fibo[i]+fibo[i+1])%1234567;
-    fibo.push(tmp)
-    console.log(fibo);
+  let fibo = [0, 1];
+  for(let i=0;i<n-1;i++){
+    let tmp = (fibo[i] + fibo[i+1]) % 1234567;
+    fibo.push(tmp);
   }
   return fibo[n]
 }
-
-let n = 5;
-console.log(solution(n));
+let n = S;
+console.log(solution(n))
