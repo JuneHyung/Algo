23dfb33f347de333e675d3b550e580359b711d58
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2023-08-07 20:27:22 +0900

    1929 소수구하기 다시풀기

--- boj/1929 소수 구하기.js
+++ boj/1929 소수 구하기.js
@@ -2,22 +2,23 @@
  * 1929 소수 구하기
  * N과 M사이 소수구하기
  */
-const fs = require('fs')
-const input = fs.readFileSync('/dev/stdin').toString().trim()
+// const fs = require('fs')
+// const input = fs.readFileSync('/dev/stdin').toString().trim()
+const input = '3 16';
 const [N, M] = input.split(' ').map(Number);
 const solution = (n, m) =>{
-  const prime = {1: true}
+  const notPrime = {1: true};
+
   for(let i=2;i<=Math.ceil(Math.sqrt(m)); i++){
-    if(prime[i]) continue;
+    if(notPrime[i]) continue;
     for(let j=i**2; j<=m;j+=i){
-      prime[j] = true;
+      notPrime[j]=true;
     }
   }
-
   const result = [];
 
   for(let i=n;i<=m;i++){
-    if(!prime[i]) result.push(i)
+    if(!notPrime[i]) result.push(i)
   }
   return result.join('\n')
 }
