621559ed6e602896c8844762ada249a9e206a376
cjh951114 <cjh951114@gmail.com>
2025-03-18 20:06:54 +0900

    [2025_재활훈련] 15650, 15651, 15652
    15650 N과 M(2)
    15651 N과 M(3)
    15652 N과 M(4)

---
+++ boj/2025_재활훈련/15652 N과 M(4).js
@@ -0,0 +1,37 @@
+/**
+ * 15651 N과 M(3)
+ * N과 M이 주어졌을 때 길이가 M인 수열 구하기.
+ * 1~N까지 자연수 중 M개를 고른 수열
+ * 같은 수를 여러 번 골라도 된다.
+ */
+/**
+ * 중복조합 구하기
+ */
+// const fs =require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim()
+const input = '4 2';
+const [N, M] = input.split(' ').map(Number);
+
+
+const solution = (n, m) => {
+  const arr = Array.from({length: n},(_,i)=>i+1);
+
+  const getCombi = (arr, m) => {
+    if(m===1) return arr.map(el=>[el]);
+    const result = [];
+
+    arr.forEach((fixed, idx, origin)=>{
+      const rest = origin.slice(idx);
+      const combi = getCombi(rest, m-1);
+      const attach = combi.map(el=>[fixed, ...el ])
+      result.push(...attach);
+    })
+    
+    return result;
+  }
+
+  const combi = getCombi(arr, m);
+  return combi.map(el=>el.join(' ')).join('\n')
+}
+
+console.log(solution(N, M));
